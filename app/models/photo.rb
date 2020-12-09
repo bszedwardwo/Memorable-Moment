82193f6d9eede2681ab2f9d4757d2a06ca750026
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-12-09 17:05:48 +0900

    photoモデルのアソシエーション設定

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,7 @@
 class Photo < ApplicationRecord
+  belongs_to :user
+  has_many :folders, through: :photo_folders
+  has_many :tags, through: :photo_tags
+  has_one :comment
+  has_one_attached :image
 end
