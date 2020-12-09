7ca4b6bf42517ab4d6a85f84ad8968e8b47f2454
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-12-09 16:09:35 +0900

    imagesテーブルのマイグレートファイルの編集

--- db/migrate/20201209070719_create_images.rb
+++ db/migrate/20201209070719_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
-
+      t.boolean :favorite,  null: false
+      t.references :user,   null: false, foreign_key: true
       t.timestamps
     end
   end
