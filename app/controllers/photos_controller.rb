0f823071a0b684af30bd3c7162214899f0cf7e82
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-12-09 18:50:16 +0900

    photosコントローラーで画像を保存できるよう設定

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,12 @@
 class PhotosController < ApplicationController
+  
   def new
-    @image = Image.new
+    @photo = Photo.new
   end
 
   def create
-    binding.pry
-    @image = Image.new
-    if @image.save
+    @photo = Photo.new(photo_params)
+    if @photo.save
       redirect_to root_path
     else
       render :new
@@ -15,7 +15,7 @@ class PhotosController < ApplicationController
 
   private
 
-  def image_params
-    params.require(:ptoho).permit(:image).merge(user_id: current_user.id)
+  def photo_params
+    params.require(:photo).permit(:image).merge(user_id: current_user.id)
   end
 end
