7b83d1c43d169b3f5958bf6704562d3885cda46a
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-12-09 17:00:36 +0900

    photosテーブルのマイグレート設定

--- db/migrate/20201209075533_create_photos.rb
+++ db/migrate/20201209075533_create_photos.rb
@@ -1,7 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-
+      t.boolean :favorite
+      t.references :user,   null: false, foreign_key: true 
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_071544) do
+ActiveRecord::Schema.define(version: 2020_12_09_075533) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,12 +33,12 @@ ActiveRecord::Schema.define(version: 2020_12_09_071544) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.boolean "favorite", null: false
+  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.boolean "favorite"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_images_on_user_id"
+    t.index ["user_id"], name: "index_photos_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -55,5 +55,5 @@ ActiveRecord::Schema.define(version: 2020_12_09_071544) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "images", "users"
+  add_foreign_key "photos", "users"
 end
