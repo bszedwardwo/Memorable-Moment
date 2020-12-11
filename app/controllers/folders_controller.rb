6e4fd1892ee5d9b54e6483f39255b43bd769fabb
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-12-11 12:13:13 +0900

    folderコントローラーにcreate機能を追加

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,24 @@
 class FoldersController < ApplicationController
+  def index
+    @folder = Folder.all.order('created_at DESC')
+  end
+
+  def new
+    @folder = Folder.new
+  end
+
+  def create
+    @folder = Folder.new(folder_params)
+    if @folder.save
+      redicrect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def folder_params
+    params.require(:folder).permit(:name).merge(user_id: current_user.id)
+  end
 end
