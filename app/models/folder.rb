7e9a57f35093a11d042fa63038752ec4c7142551
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-12-11 11:24:02 +0900

    folderモデルのアソシエーション設定

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,2 +1,5 @@
 class Folder < ApplicationRecord
+  belongs_to :user
+  has_many :photos, through: :photo_folders
+  has_many :tags, through: :folder_tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_075533) do
+ActiveRecord::Schema.define(version: 2020_12_11_021452) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2020_12_09_075533) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "folders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "favorite"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_folders_on_user_id"
   end
 
   create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -56,5 +65,6 @@ ActiveRecord::Schema.define(version: 2020_12_09_075533) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "folders", "users"
   add_foreign_key "photos", "users"
 end
