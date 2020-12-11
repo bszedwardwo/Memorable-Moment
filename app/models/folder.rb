a2bdcd9a02f7bb76487f9771d208e1f7e03520f6
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-12-11 12:12:22 +0900

    folderモデルのバリデーション設定

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -3,4 +3,6 @@ class Folder < ApplicationRecord
   has_many :photo_folders
   has_many :photos, through: :photo_folders
   has_many :tags, through: :folder_tags
+
+  validates :name, presence: true
 end
