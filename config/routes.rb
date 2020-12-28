c30a09c99dcecd212b89895f82e6b61d9097ac8a
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2020-12-28 16:28:42 +0900

    show routesの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
  
-  resources :photos, only: [:new, :create, :index]
-  resources :folders, only: [:new, :create, :index]
+  resources :photos, only: [:new, :create, :index, :show]
+  resources :folders, only: [:new, :create, :index, :show]
 end
