a9b9073601a537eae0a141d9cf76e805c17b6fe2
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-01 19:31:20 +0900

    photosをfoldersにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'users#index'
  
   resources :photos, only: [:new, :create, :index, :show]
-  resources :folders, only: [:new, :create, :index, :show]
+  resources :folders, only: [:new, :create, :index, :show] do
+    resources :photos, only: [:index, :new, :create]
+  end
 end
