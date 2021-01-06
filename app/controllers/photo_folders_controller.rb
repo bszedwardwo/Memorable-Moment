d1fd3acc8468d3ddbd651af74bc4cc5c3823532c
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-06 20:34:46 +0900

    条件分岐追加

--- app/controllers/photo_folders_controller.rb
+++ app/controllers/photo_folders_controller.rb
@@ -1,9 +1,12 @@
 class PhotoFoldersController < ApplicationController
+
   def create
+    if params.present?
     folder_data = params[folder[]]
     folder_data.each do |d|
       photo = Photo.find(d)
       Folder.find(d.id).photos << photo
+    end
     end
   end
 end
