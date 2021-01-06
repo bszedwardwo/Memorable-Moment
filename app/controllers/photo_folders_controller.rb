733e24446bb0a4a612cbe582d42a736f5d6c86af
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-06 20:31:45 +0900

    コントローラー変更

--- app/controllers/photo_folders_controller.rb
+++ app/controllers/photo_folders_controller.rb
@@ -2,8 +2,8 @@ class PhotoFoldersController < ApplicationController
   def create
     folder_data = params[folder[]]
     folder_data.each do |d|
-      photo1 = Photo.find(d)
-      Folder.find(3).photos << photo1
+      photo = Photo.find(d)
+      Folder.find(3).photos << photo
     end
   end
 end
