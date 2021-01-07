2bdc0e5b54ebe63e631202ef351b88a2af9b6036
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-07 23:13:26 +0900

    データの取得

--- app/controllers/photo_folders_controller.rb
+++ app/controllers/photo_folders_controller.rb
@@ -1,8 +1,9 @@
 class PhotoFoldersController < ApplicationController
 
   def create
+    binding.pry
     if params.present?
-    folder_data = params[folder[]]
+    folder_data = params[:folder[]]
     folder_data.each do |d|
       photo = Photo.find(d)
       Folder.find(d.id).photos << photo
