12673538477a93e7159a5bc0dcb8d2051043eaaa
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-07 23:33:58 +0900

    中間テーブルにデータを追加

--- app/controllers/photo_folders_controller.rb
+++ app/controllers/photo_folders_controller.rb
@@ -2,12 +2,10 @@ class PhotoFoldersController < ApplicationController
 
   def create
     binding.pry
-    # if params.present?
-    # folder_data = params[:folder[]]
-    # folder_data.each do |d|
-    #   photo = Photo.find(d)
-    #   Folder.find(d.id).photos << photo
-    # end
-    # end
+    folder_data = params[:folder_photo]
+    folder_data.each do |d|
+      photo = Photo.find(d)
+      # Folder.find(3).photos << photo
+    end
   end
 end
