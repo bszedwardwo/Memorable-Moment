c185c49f847447db7fe0a59c7824e2debcab768c
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-07 23:16:06 +0900

    送信データの確認

--- app/controllers/photo_folders_controller.rb
+++ app/controllers/photo_folders_controller.rb
@@ -2,12 +2,12 @@ class PhotoFoldersController < ApplicationController
 
   def create
     binding.pry
-    if params.present?
-    folder_data = params[:folder[]]
-    folder_data.each do |d|
-      photo = Photo.find(d)
-      Folder.find(d.id).photos << photo
-    end
-    end
+    # if params.present?
+    # folder_data = params[:folder[]]
+    # folder_data.each do |d|
+    #   photo = Photo.find(d)
+    #   Folder.find(d.id).photos << photo
+    # end
+    # end
   end
 end
