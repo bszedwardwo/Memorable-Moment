efff99388014ed3ff0817166e0e05c02755af2dc
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-08 21:40:02 +0900

    photo_foldersコントローラー削除

--- app/controllers/photo_folders_controller.rb
+++ app/controllers/photo_folders_controller.rb
@@ -1,11 +1,4 @@
 class PhotoFoldersController < ApplicationController
 
-  def create
-    binding.pry
-    folder_data = params[:folder_photo]
-    folder_data.each do |d|
-      photo = Photo.find(d)
-      Folder.find(3).photos << photo
-    end
-  end
+
 end
