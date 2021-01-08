fcb09d50eb0a515a7609c2f56f7b2b8ceb1eb4c3
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-08 21:49:37 +0900

    Update add_photos_to_folders_controller.rb

--- app/controllers/add_photos_to_folders_controller.rb
+++ app/controllers/add_photos_to_folders_controller.rb
@@ -2,6 +2,7 @@ class AddPhotosToFoldersController < ApplicationController
   def create
     folder_data = params[:folder_photo]
     folder_data.each do |d|
+      photo = Photo.find(d)
     end
   end
 end
