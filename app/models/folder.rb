030995846c874865d6976a8640aba5eb1dd720bb
bszedwardwo <73581167+bszedwardwo@users.noreply.github.com>
2021-01-09 22:42:23 +0900

    Update folder.rb

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,8 +1,8 @@
 class Folder < ApplicationRecord
-  # belongs_to :user
-  # has_many :photo_folders
-  # has_many :photos, through: :photo_folders
-  # has_many :tags, through: :folder_tags
+  belongs_to :user
+  has_many :photo_folders
+  has_many :photos, through: :photo_folders
+  has_many :tags, through: :folder_tags
 
   validates :name, presence: true
 end
